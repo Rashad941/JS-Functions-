346d9f9ffe31e2aebeac35ab3470b7f4851574b7
Michael Ethan Rashad-Nakpan <methan941@gmail.com>
2025-04-14 22:11:09 +0000

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,18 @@
+
+const testFunction (age) => {
+    if (age <= 15) {
+        console.log ('You are eligible to participate');
+    }
+}
+
+testFunction(15)
+
+
+const username = prompt ('What is your name?');
+
+function users() {
+    if (username === 'Rashad') {
+        console.log (`Welcome ${username}`)
+    }
+}
+ users();
